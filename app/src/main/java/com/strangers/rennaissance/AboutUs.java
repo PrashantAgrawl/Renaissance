f56bce3632bd32e2f3cab3ba6eca17b51ef5594e
strangers <prashantagrawal.mnnit@gmail.com>
2015-12-15 03:07:41 +0530

    About us implemented

--- app/src/main/java/com/strangers/rennaissance/AboutUs.java
+++ app/src/main/java/com/strangers/rennaissance/AboutUs.java
@@ -3,18 +3,37 @@ package com.strangers.rennaissance;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by root on 15/12/15.
  */
 public class AboutUs extends Fragment {
+    private static int mSelectedPosition;
+    private LinearLayout linearLayout;
+    private static String TAG="About Us";
+    public static AboutUs newInstance(int position) {
+        mSelectedPosition=position;
+        AboutUs fragment = new AboutUs();
+        Bundle args = new Bundle();
+        args.putInt("Position", position);
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_us,container,false);
+
+        if((linearLayout=(LinearLayout)inflater.inflate(R.layout.about_us,container,false))==null)
+            Log.d(TAG,"linear null");
+        TextView tv=(TextView)linearLayout.findViewById(R.id.fragText);
+        tv.setText("Teri maa ki aankh");
+        return linearLayout;
     }
 
 }
